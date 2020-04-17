f6c87e289ed0bcf698752a4debe74d3b7e188f84
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-17 10:03:16 +0530

    Setting Up A Spring Boot Project

---
+++ spring-basic-steps/src/main/java/com/spring/basics/springbasicsteps/SpringBasicStepsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basics.springbasicsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicStepsApplication.class, args);
+	}
+
+}
---
+++ spring-basic-steps/src/test/java/com/spring/basics/springbasicsteps/SpringBasicStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basics.springbasicsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
