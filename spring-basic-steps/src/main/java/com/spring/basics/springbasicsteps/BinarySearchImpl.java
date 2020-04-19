5b4fec0c25d3feb41d06c14acbfea3c454e661d4
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-19 12:18:07 +0530

    Lifecycle Of A Bean - @PostConstruct And @PreDestroy

--- spring-basic-steps/src/main/java/com/spring/basics/springbasicsteps/BinarySearchImpl.java
+++ spring-basic-steps/src/main/java/com/spring/basics/springbasicsteps/BinarySearchImpl.java
@@ -1,11 +1,17 @@
 package com.spring.basics.springbasicsteps;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl{
+	private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	@Qualifier("quick")
 	SortAlgorithm sortAlgorithm;
@@ -17,5 +23,14 @@ public class BinarySearchImpl{
 //	}
 	public void binarySearch() {
 		sortAlgorithm.sort();
+	}
+	@PostConstruct
+	public void postConstruct() {
+		LOGGER.info("From post construct");
+	}
+	
+	@PreDestroy
+	public void preDestroy() {
+		LOGGER.info("From pre destroy");
 	}
 }
