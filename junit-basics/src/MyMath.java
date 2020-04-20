9fa1ff485377bb0a5aafbb32c839bb8ef15ad1a8
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-20 18:34:56 +0530

    First Junit Project & Green Bar

---
+++ junit-basics/src/MyMath.java
@@ -0,0 +1,12 @@
+
+public class MyMath {
+	int sum(int[] numbers) {
+		int sum = 0;
+		for(int i: numbers) {
+			sum += i;
+		}
+		return sum;
+		
+	}
+
+}
---
+++ junit-basics/test/MyMathTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void test() {
+		//The absence of failure is success.
+	}
+
+}
