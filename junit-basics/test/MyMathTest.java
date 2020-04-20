7751bbfd5fe3f90b69e0d3d0e673f58a7f4e606b
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-20 18:45:05 +0530

    First Code & First Unit Test

--- junit-basics/test/MyMathTest.java
+++ junit-basics/test/MyMathTest.java
@@ -1,12 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
-
+	MyMath myMath = new MyMath();
 	@Test
-	void test() {
-		//The absence of failure is success.
+	void sum_with3numbers() {	
+		assertEquals(6, myMath.sum(new int[] {1,2,3}));		
+	}
+	@Test
+	void sum_with1number() {	
+		assertEquals(3, myMath.sum(new int[] {3}));		
 	}
 
 }
