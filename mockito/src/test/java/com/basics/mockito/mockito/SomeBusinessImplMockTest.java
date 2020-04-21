97d07774d3d5c8f7128654add70ef62df53f74aa
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-21 13:07:59 +0530

    Using Mockito Annotations - @Mock, @InjectMocks, @RunWith

--- mockito/src/test/java/com/basics/mockito/mockito/SomeBusinessImplMockTest.java
+++ mockito/src/test/java/com/basics/mockito/mockito/SomeBusinessImplMockTest.java
@@ -1,14 +1,21 @@
 package com.basics.mockito.mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+@RunWith(MockitoJUnitRunner.class)
 class SomeBusinessImplMockTest {
-	DataService dataServiceMock = mock(DataService.class);
-	SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceMock);
+	@Mock
+	DataService dataServiceMock;
+	
+	@InjectMocks
+	SomeBusinessImpl someBusinessImpl;
+	
 	@Test
 	void testWith3Numbers() { 
 		when(dataServiceMock.retrieveAllData()).thenReturn( new int[] {20,32,40});		
