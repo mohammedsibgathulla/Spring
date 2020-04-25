ca355e4b2f1143469e21dd75fb25a0e995e60ce7
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-25 20:54:53 +0530

    Using @Around Advice To Implement Performance Tracing

---
+++ spring-aop/src/main/java/com/spring/aop/springaop/aspect/AroundAspect.java
@@ -0,0 +1,24 @@
+package com.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+//The combination of Advice & Pointcut is called an Aspect.
+@Aspect
+@Configuration
+public class AroundAspect {
+	private org.slf4j.Logger logger =  LoggerFactory.getLogger(this.getClass());
+	
+	@Around(
+			value="execution(* com.spring.aop.springaop.business.*.*(..))")
+	public void after(ProceedingJoinPoint proceedingJointPoint) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		proceedingJointPoint.proceed();
+		long timeTaken = System.currentTimeMillis() - startTime;
+		
+		logger.info("Time taken by {} is {}",proceedingJointPoint,  timeTaken);
+	}
+
+}
