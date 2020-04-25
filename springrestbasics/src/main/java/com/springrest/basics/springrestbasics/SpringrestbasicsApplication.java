2774231b3e7bc2bc465b59f072876ded9258b77f
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-25 15:20:11 +0530

    What Is Spring Boot Auto Configuration

--- springrestbasics/src/main/java/com/springrest/basics/springrestbasics/SpringrestbasicsApplication.java
+++ springrestbasics/src/main/java/com/springrest/basics/springrestbasics/SpringrestbasicsApplication.java
@@ -2,12 +2,16 @@ package com.springrest.basics.springrestbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringrestbasicsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringrestbasicsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringrestbasicsApplication.class, args);
+		//The run method also returns ApplicationContext
+		for(String name: applicationContext.getBeanDefinitionNames())
+		System.out.println(name);
 	}
 
 }
