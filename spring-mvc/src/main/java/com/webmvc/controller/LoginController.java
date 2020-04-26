b459d4c2ce0e12cfbfbda37b678f06190aaaf043
mohammed, sibgathulla <mohammed.shaik81@gmail.com>
2020-04-26 22:44:16 +0530

    Your First Spring MVC Controller

---
+++ spring-mvc/src/main/java/com/webmvc/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.webmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value="/login")
+	@ResponseBody
+	public String sayHell() {
+		return "Hellow World Duffer";
+	}
+}
